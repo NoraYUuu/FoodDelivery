375465ba407ebab3f92914dd683dc87dd4c877e8
Huaiyu <norafish79@gmail.com>
2021-05-10 17:48:00 +0800

    day2

---
+++ components/Search/Search.js
@@ -0,0 +1,23 @@
+// components/Search/Search.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
