da5eccc11709f9212a8e295faf9e1798279166c5
NoraYUuu <46596402+NoraYUuu@users.noreply.github.com>
2021-06-22 19:41:19 +0800

    创建新拼团
    merge with simon

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,47 +2,39 @@
 // 获取应用实例
 const app = getApp()
 
+import { request } from "../../request/index.js";
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    // 轮播图数组
+    swiperList: [],
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  // 页面开始加载 就会触发
+  onLoad: function (options) {
+    // // 1 发送异步请求获取轮播图数据  优化的手段可以通过es6的 promise来解决这个问题 
+    // wx.request({
+    //   url: 'https://haoyusimon.github.io/Data/index_banner.json',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   }
+    // });
+    
+    this.getSwiperList();
+    // this.getCateList();
+    // this.getFloorList();
+      
   },
-  onLoad() {
-    if (wx.getUserProfile) {
+
+  // 获取轮播图数据
+  getSwiperList(){
+    request({ url: "/Data/index_banner.json" })
+    .then(result => {
       this.setData({
-        canIUseGetUserProfile: true
+        swiperList: result
       })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
+  // 获取 分类导航数据
 })
